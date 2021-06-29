0802ca0ffe1f42f4d7c2c2c197d460202f7db134
Brian Adams <bpeteradams@gmail.com>
2021-06-29 12:35:51 -0400

    refactored Landing Page as a React componenet

---
+++ react-portfolio/src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+     <div class="jumbotron text-center">'
+        <img src="./assets/images/profilepic.jpeg" alt="" class="profile-pic my-3" />
+        <p class="lead">Web Developer. Music, Skiing, and Dungeons and Dragons Enthusiast</p>
+        <hr class="my-4" />
+        <p>Eager to Learn</p>
+        <a class="btn btn-lg landing-page-btn" href="./assets/html/about-me.html" role="button">Learn More</a>
+    </div>
+  )
+}
+
+export default LandingPage;
